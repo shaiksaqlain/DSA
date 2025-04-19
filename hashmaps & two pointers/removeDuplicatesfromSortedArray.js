2135c2505dc190b012e9a0c73721f353feea52ac
Shaik Saqlain <shaik.saqlain@neosofttech..com>
2025-04-19 22:38:01 +0530

    variable updated

--- hashmaps & two pointers/removeDuplicatesfromSortedArray.js
+++ hashmaps & two pointers/removeDuplicatesfromSortedArray.js
@@ -46,15 +46,15 @@ const nums = [0, 0, 1, 1, 2, 2, 3, 3, 4]
 
 
 var removeDuplicates = function (nums) {
-    let l = 1;
-    for (let r = 1; r < nums.length; r++) {
-        if (nums[r] != nums[r - 1]) {
-            nums[l] = nums[r]
-            l++
+    let left = 1
+    for (let right = 1; right < nums.length; right++) {
+        if (nums[right] != nums[right - 1]) {
+            nums[left] = nums[right]
+            left++
         }
+
     }
-    
-    return l;
+    return left;
 };
 
 console.log(removeDuplicates(nums))
