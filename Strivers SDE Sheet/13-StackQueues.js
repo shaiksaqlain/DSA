6eed81f0dc40815cb0fe1bdb80382a56079e08de
Shaik Saqlain <shaik.saqlain@neosofttech..com>
2025-08-23 14:11:59 +0530

    queue implemention

--- Strivers SDE Sheet/13-StackQueues.js
+++ Strivers SDE Sheet/13-StackQueues.js
@@ -34,28 +34,28 @@ class Stack {
     }
 }
 
-const stack = new Stack();
+// const stack = new Stack();
 
-stack.push(1)
-stack.push(2)
-stack.push(3)
-stack.push(4)
-stack.push(5)
-stack.push(6)
-stack.push(7)
-stack.push(8)
-stack.push(9)
-stack.push(10)
-stack.push(11)
-stack.printStack()
+// stack.push(1)
+// stack.push(2)
+// stack.push(3)
+// stack.push(4)
+// stack.push(5)
+// stack.push(6)
+// stack.push(7)
+// stack.push(8)
+// stack.push(9)
+// stack.push(10)
+// stack.push(11)
+// stack.printStack()
 
-stack.pop()
-stack.pop()
-stack.pop()
-stack.pop()
-stack.pop()
-stack.printStack()
-console.log(stack.peek());
+// stack.pop()
+// stack.pop()
+// stack.pop()
+// stack.pop()
+// stack.pop()
+// stack.printStack()
+// console.log(stack.peek());
 
 //##########################################################  Queue implemention ###############################################
 
@@ -68,62 +68,71 @@ class Queue {
     }
 
     enQueue(data) {
-        if ((this.end + 1) % this.size == this.start) {
-            return console.log('Queue is overflow!');
+        if (((this.end + 1) % this.size) == this.start) {
+            return console.log("Queue overflow!");
         }
         if (this.start == -1) {
-            this.start = 0;
+            this.start = 0
         }
-        this.queue[++this.end] = data;
+        this.end = (this.end + 1) % this.size
+        this.queue[this.end] = data;
     }
     deQueue() {
-        if ((this.start + 1) % this.end == 0) {
-            return console.log('Queue is empty!');
+        if (((this.start + 1) % this.end == 0)) {
+            return console.log("Queue Empty!");
         }
         const removed = this.queue[this.start];
         this.queue[this.start] = null;
-        this.start++;
+
+        if (this.start == this.end) {
+            this.start = this.end = -1;
+        } else {
+            this.start = ((this.start + 1) % this.size)
+        }
+
         return removed;
     }
     top() {
-        return this.start === -1 ? null : console.log(this.queue[this.start]);
-        ;
+        return this.start == -1 ? null : console.log(this.queue[this.start]);
     }
 
     printQueue() {
-        if (this.start == -1) return console.log('Queue is empty');
+        if (this.start == -1) return console.log("Queue Empty!");
+
         let i = this.start;
-        const result = []
-        
+        const result = [];
         while (true) {
-            result.push(this.queue[i]);
+            result.push(this.queue[i])
             if (i == this.end) break;
             i = ((i + 1) % this.size)
         }
         console.log(result);
-        
     }
 
 }
 
-const myQueue = new Queue();
-myQueue.enQueue(1)
-myQueue.enQueue(2)
-myQueue.enQueue(3)
-myQueue.enQueue(4)
-myQueue.enQueue(5)
-myQueue.enQueue(6)
-myQueue.enQueue(7)
-myQueue.enQueue(8)
-myQueue.enQueue(9)
-myQueue.enQueue(10)
-myQueue.enQueue(11)
-myQueue.enQueue(11)
-myQueue.enQueue(11)
+// const myQueue = new Queue();
+// myQueue.enQueue(1)
+// myQueue.enQueue(2)
+// myQueue.enQueue(3)
+// myQueue.enQueue(4)
+// myQueue.enQueue(5)
+// myQueue.enQueue(6)
+// myQueue.enQueue(7)
+// myQueue.enQueue(8)
+// myQueue.enQueue(9)
+// myQueue.enQueue(10)
+// myQueue.enQueue(11)
+// myQueue.enQueue(11)
+// myQueue.enQueue(11)
+// myQueue.enQueue(11)
 
-myQueue.printQueue()
-myQueue.deQueue()
-myQueue.deQueue()
-myQueue.deQueue()
-myQueue.printQueue()
-myQueue.top();
+// myQueue.printQueue()
+// myQueue.deQueue()
+// myQueue.deQueue()
+// myQueue.deQueue()
+// myQueue.enQueue(1)
+// myQueue.enQueue(2)
+// myQueue.enQueue(3)
+// myQueue.printQueue()
+// myQueue.top();
