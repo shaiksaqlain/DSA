c6cd30fbb285866e1e115cc3bc163c61e36da7de
Shaik Saqlain <shaik.saqlain@neosofttech..com>
2025-08-26 11:24:28 +0530

    sliding window done

--- Strivers SDE Sheet/14-StackQueues2.js
+++ Strivers SDE Sheet/14-StackQueues2.js
@@ -57,12 +57,91 @@ class LRUCache {
 
 }
 
-const cache = new LRUCache(3);
-cache.put(1, "A");
-cache.put(2, "B");
-cache.put(3, "C");
-console.log(cache.get(2)); // "B" (2 becomes most recent)
-cache.put(4, "D");         // Removes 1 (least recently used)
-console.log(cache.get(1)); // -1 (1 was removed)
-console.log(cache.get(3)); // "C"
-console.log(cache.get(4)); // "D"
+// const cache = new LRUCache(3);
+// cache.put(1, "A");
+// cache.put(2, "B");
+// cache.put(3, "C");
+// console.log(cache.get(2)); // "B" (2 becomes most recent)
+// cache.put(4, "D");         // Removes 1 (least recently used)
+// console.log(cache.get(1)); // -1 (1 was removed)
+// console.log(cache.get(3)); // "C"
+// console.log(cache.get(4)); // "D"
+
+
+//########################################################## Largest rectangle in a histogram ###############################################
+function largestRectangleAreaV1(heights) {
+    const rightSmaller = [];
+    const leftSmaller = [];
+    let stack = [];
+    let result = 0;
+    const n = heights.length;
+
+    for (let i = n - 1; i >= 0; i--) {
+        while (stack.length && heights[stack[stack.length - 1]] >= heights[i]) {
+            stack.pop();
+        }
+        rightSmaller[i] = !stack.length ? n : stack[stack.length - 1]
+        stack.push(i)
+    }
+    stack = [];
+    for (let i = 0; i < n; i++) {
+        while (stack.length && heights[stack[stack.length - 1]] >= heights[i]) {
+            stack.pop();
+        }
+        leftSmaller[i] = !stack.length ? -1 : stack[stack.length - 1]
+        stack.push(i)
+    }
+
+    for (let i = 0; i < n; i++) {
+        const width = rightSmaller[i] - leftSmaller[i] - 1;
+        const area = heights[i] * width;
+        result = Math.max(result, area);
+    }
+    return result
+}
+
+function largestRectangleAreaV2(heights) {
+    let stack = [];
+    let result = 0;
+
+    for (let i = 0; i <= heights.length; i++) {
+        const currentHeight = (i == heights.length) ? 0 : heights[i];
+        while (stack.length && currentHeight <= heights[stack[stack.length - 1]]) {
+            const topIndex = stack.pop();
+            const height = heights[topIndex];
+            const width = stack.length === 0 ? i : i - stack[stack.length - 1] - 1;
+            result = Math.max(result, height * width)
+        }
+        stack.push(i)
+    }
+    return result
+}
+
+// const heights = [2, 1, 5, 6, 2, 3];
+// console.log(largestRectangleAreaV2(heights));
+
+//########################################################## Sliding Window Maximum ###############################################
+
+function slidingWindowMaximum(array, k) {
+    const deque = [];
+    const result = [];
+    for (let i = 0; i < array.length; i++) {
+        if (deque.length && deque[0] === i - k) {
+            deque.shift();
+        }
+        while (deque.length && array[deque[deque.length - 1]] < array[i]) {
+            deque.pop();
+        }
+        deque.push(i);
+        if (i >= k - 1) {
+            result.push(array[deque[0]]);
+        }
+        console.log(deque);
+
+    }
+    return result;
+}
+
+
+const array = [4, 0, -1, 3, 5, 3, 6, 8], k = 3;
+console.log(slidingWindowMaximum(array, k));
